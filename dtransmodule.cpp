cdbeaf812265b51748d3ba7dd9909d5407053fdd
poftwaresatent <poftwaresatent@255146f7-950e-0410-bd4e-f0acf59374a5>
2010-11-17 23:02:05 +0000

    whitespace tweaks
    git-svn-id: https://estar.svn.sourceforge.net/svnroot/estar/trunk/dtrans@332 255146f7-950e-0410-bd4e-f0acf59374a5

--- dtransmodule.cpp
+++ dtransmodule.cpp
@@ -321,26 +321,26 @@ static PyMethodDef dtrans_methods[] = {
 
 static PyTypeObject dtrans_type = {
   PyObject_HEAD_INIT(NULL)
-  0,                                        /* ob_size*/
-  "dtrans.DistanceTransform",               /* tp_name*/
-  sizeof(dtrans_object),                    /* tp_basicsize*/
-  0,                                        /* tp_itemsize*/
-  (destructor) dtrans_dealloc,              /* tp_dealloc*/
-  0,                                        /* tp_print*/
-  0,                                        /* tp_getattr*/
-  0,                                        /* tp_setattr*/
-  0,                                        /* tp_compare*/
-  0,                                        /* tp_repr*/
-  0,                                        /* tp_as_number*/
-  0,                                        /* tp_as_sequence*/
-  0,                                        /* tp_as_mapping*/
-  0,                                        /* tp_hash */
-  0,                                        /* tp_call*/
-  0,                                        /* tp_str*/
-  0,                                        /* tp_getattro*/
-  0,                                        /* tp_setattro*/
-  0,                                        /* tp_as_buffer*/
-  Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE, /* tp_flags*/
+  0,                                        /* ob_size */
+  "dtrans.DistanceTransform",               /* tp_name */
+  sizeof(dtrans_object),                    /* tp_basicsize */
+  0,                                        /* tp_itemsize */
+  (destructor) dtrans_dealloc,              /* tp_dealloc */
+  0,                                        /* tp_print */
+  0,                                        /* tp_getattr */
+  0,                                        /* tp_setattr */
+  0,                                        /* tp_compare */
+  0,                                        /* tp_repr */
+  0,                                        /* tp_as_number */
+  0,                                        /* tp_as_sequence */
+  0,                                        /* tp_as_mapping */
+  0,                                        /* tp_hash  */
+  0,                                        /* tp_call */
+  0,                                        /* tp_str */
+  0,                                        /* tp_getattro */
+  0,                                        /* tp_setattro */
+  0,                                        /* tp_as_buffer */
+  Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE, /* tp_flags */
   "DistanceTransform type",                 /* tp_doc */
   0,                                        /* tp_traverse */
   0,                                        /* tp_clear */
